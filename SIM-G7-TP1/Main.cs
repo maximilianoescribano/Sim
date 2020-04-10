b4b101fe16b94a1a38e7ff610e4ea7f3bd88a74e
maximilianoescribano <maximilianoescribano@gmail.com>
2020-04-10 12:57:16 -0300

    first commit

---
+++ SIM-G7-TP1/Main.cs
@@ -0,0 +1,158 @@
+﻿using SIM_G7_TP1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SIM_G7_TP1
+{
+    public partial class Main : Form
+    {
+        double[] randomNumbers;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            switch (tabRandomMethods.SelectedIndex)
+            {
+                case 0:
+                    generateCongrLineal();
+                    break;
+                case 1:
+                    generateCongrMulti();
+                    break;
+                case 2:
+                   generateLanguageRandom();
+                   break;
+            }
+        }
+
+        private void generateLanguageRandom()
+        {
+            int rndNumCount = Convert.ToInt16(nudRandomNumbersCount.Value);
+            int seed = Convert.ToInt16(nudLangSeed.Value);
+            RandomGenerator rndGen = new RandomGenerator(seed);
+
+            randomNumbers = rndGen.generateLangRandom(rndNumCount);
+            fillDGNumbers(randomNumbers);
+            generateFrecuencies();
+        }
+
+        private void generateCongrLineal()
+        {
+            int rndNumCount = Convert.ToInt16(nudRandomNumbersCount.Value);
+            int seed = Convert.ToInt16(nudCongrLinealSemilla.Value);
+            int constMulti = Convert.ToInt16(nudCongrLinealConstMulti.Value);
+            int constAditiv = Convert.ToInt16(nudCongrLinealConstAditiva.Value);
+            int magMod = Convert.ToInt16(nudCongrLinealMagnitudModulo.Value);
+
+            RandomGenerator rndGen = new RandomGenerator(seed, constMulti, magMod, constAditiv);
+
+            randomNumbers = rndGen.generateCongrLinealRandom(rndNumCount);
+            fillDGNumbers(randomNumbers);
+            generateFrecuencies();
+        }
+
+        private void generateCongrMulti()
+        {
+            int rndNumCount = Convert.ToInt16(nudRandomNumbersCount.Value);
+            int seed = Convert.ToInt16(nudCongrMultiSeed.Value);
+            int constMulti = Convert.ToInt16(nudCongrMultiConstMulti.Value);
+            int magMod = Convert.ToInt16(nudCongrMultiMagnitudModulo.Value);
+
+            RandomGenerator rndGen = new RandomGenerator(seed, constMulti, magMod, 0);
+
+            randomNumbers = rndGen.generateCongrMultiRandom(rndNumCount);
+            fillDGNumbers(randomNumbers);
+            generateFrecuencies();
+        }
+
+        private void generateFrecuencies()
+        {
+            RandomGenerator gen = new RandomGenerator();
+            int numIntervals = Convert.ToInt16(nudNumInvervals.Value);
+
+            double[,] frecuencias = gen.validateFrecuencies(randomNumbers, numIntervals);
+
+            fillDBFrecuencies(frecuencias);
+            gradlib.Visible = true;
+            gradlib.Text = $"Grados de Libertad = {(frecuencias.GetLength(0) - 1)}";
+            fillChart(frecuencias);
+        }
+
+        private void fillDGNumbers(double[] num)
+        {
+            dtgNumeros.Rows.Clear();
+            for (int i = 0; i < num.Length; i++)
+            {
+                dtgNumeros.Rows.Add(i + 1, num[i]);
+            }
+        }
+
+        private void fillDBFrecuencies(double[,] frec)
+        {
+            dtgIntervalos.Rows.Clear();
+            String intervalo = "";
+
+            for (var i = 0; i < frec.GetLength(0); i++)
+            {
+                intervalo = $"{frec[i, 0]} - {frec[i, 1]}";
+                dtgIntervalos.Rows.Add(intervalo, frec[i, 2], frec[i, 3], frec[i, 4], frec[i, 5]);
+            }
+
+        }
+
+        private void fillChart(double[,] frec)
+        {
+            String intervalo;
+
+            graficoObtenida.Series[0].Points.Clear();
+            graficoObtenida.Series[1].Points.Clear();
+
+            for (int f = 0; f < frec.GetLength(0); f++)
+            {
+                intervalo = $"{frec[f, 1]} - {frec[f, 0]}";
+                graficoObtenida.Series["Observada"].Points.AddXY(frec[f, 1], frec[f, 2]);
+                graficoObtenida.Series["Esperada"].Points.AddXY(frec[f, 1], frec[f, 3]);
+                graficoObtenida.Series["Observada"].Points[f].AxisLabel = intervalo;
+            }
+
+            graficoObtenida.Series["Observada"].ChartType = SeriesChartType.Column;
+            graficoObtenida.Series["Observada"].XValueType = ChartValueType.Double;
+            graficoObtenida.Series["Observada"].YValueType = ChartValueType.Int32;
+            graficoObtenida.ChartAreas[0].AxisX.LabelStyle.Interval = (frec[0, 1] - frec[0, 0]/2);
+            graficoObtenida.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            //graficoObtenida.ChartAreas[0].AxisX.ScaleView.Zoom(0, 0.5);
+            graficoObtenida.Series["Observada"].IsVisibleInLegend = true;
+           
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dtgNumeros.Rows.Clear();
+            graficoObtenida.Series[0].Points.Clear();
+            graficoObtenida.Series[1].Points.Clear();
+            gradlib.Visible = false;
+        }
+
+        private void generateTestChi()
+        {
+            randomNumbers = new[]
+            {
+                0.15, 0.22, 0.41, 0.65, 0.84, 0.81, 0.62, 0.45, 0.32, 0.07, 0.11, 0.29, 0.58, 0.73, 0.93, 0.97, 0.79,
+                0.55, 0.35, 0.09, 0.99, 0.51, 0.35, 0.02, 0.19, 0.24, 0.98, 0.10, 0.31, 0.17
+            };
+            fillDGNumbers(randomNumbers);
+            generateFrecuencies();
+        }
+    }
+}
---
+++ SIM-G7-TP1/Models/RandomGenerator.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIM_G7_TP1.Models
+{
+    public class RandomGenerator
+    {
+        private int _a;
+        private int _c;
+        private int _m;
+        private int _semilla;
+        private double _rnd;
+        private double _chi2;
+        private double _menorValor;
+        private double _mayorValor;
+
+        public int A
+        {
+            get { return _a; }
+            set { _a = value; }
+        }
+
+        public int C
+        {
+            get { return _c; }
+            set { _c = value; }
+        }
+
+        public int M
+        {
+            get { return _m; }
+            set { _m = value; }
+        }
+
+        public int Semilla
+        {
+            get { return _semilla; }
+            set { _semilla = value; }
+        }
+
+        public double Chi2
+        {
+            get { return _chi2; }
+            set { _chi2 = value; }
+        }
+
+        public double MenorValor
+        {
+            get { return _menorValor; }
+            set { _menorValor = value; }
+        }
+
+        public double MayorValor
+        {
+            get { return _mayorValor; }
+            set { _mayorValor = value; }
+        }
+
+        public RandomGenerator() : this(0)
+        {
+        }
+
+        public RandomGenerator(int semilla) : this(semilla, 0, 0, 0)
+        {
+        }
+
+        public RandomGenerator(int semilla, int a, int m, int c)
+        {
+            _a = a;
+            _c = c;
+            _m = m;
+            _semilla = semilla;
+            _rnd = 0;
+            _chi2 = 0;
+            _menorValor = 0;
+            _mayorValor = 0;
+        }
+
+        public double[] generateLangRandom(int count)
+        {
+            var rnd = new Random(Semilla);
+            var nums = new double[count];
+
+            for (var i = 0; i < count; i++)
+            {
+                nums[i] = Math.Round(rnd.NextDouble(), 4);
+            }
+
+            return nums;
+        }
+
+        public double[] generateCongrLinealRandom(int count)
+        {
+            double[] nums = new double[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int newSeed = generateSeed();
+                double rnd = ((double)newSeed) / (M - 1);
+
+                nums[i] = Math.Round(rnd, 4);
+            }
+
+            return nums;
+        }
+
+        private int generateSeed()
+        {
+            int Xn;
+
+            Xn = Semilla * A + C;
+
+            Semilla = Xn % M;
+
+            return Semilla;
+        }
+
+        public double[] generateCongrMultiRandom(int count)
+        {
+            double[] nums = new double[count];
+            C = 0;
+
+            nums = generateCongrLinealRandom(count);
+
+            return nums;
+        }
+
+
+        public double[,] validateFrecuencies(double[] nums, int intervals)
+        {
+            double[,] frecuencies = new double[intervals, 6];
+            double low = 0;
+            double high = 0;
+            double frecEsp = 0;
+            int frecObs = 0;
+            double calcEst = 0;
+            double calcEstAcum = 0;
+
+            for (int i = 0; i < intervals; i++)
+            {
+                low = high;
+                var high1 = Math.Round(((double)1 / intervals), 4);
+                high = Math.Round(low + high1,2);
+                frecEsp = ((double)nums.Length) / intervals;
+                frecObs = countNumbersWithinInterval(low, high, nums);
+                calcEst = Math.Round(Math.Pow(frecObs - frecEsp, 2) / frecEsp, 4);
+                calcEstAcum += calcEst;
+
+                frecuencies[i, 0] = low;
+                frecuencies[i, 1] = high;
+                frecuencies[i, 2] = frecObs;
+                frecuencies[i, 3] = frecEsp;
+                frecuencies[i, 4] = calcEst;
+                frecuencies[i, 5] = calcEstAcum;
+            }
+
+            return frecuencies;
+        }
+
+        public int countNumbersWithinInterval(double low, double high, double[] nums)
+        {
+            return nums.Count(item => item >= low && item <= high);
+        }
+    }
+}
